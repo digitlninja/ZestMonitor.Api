97aa63756d3442a2fc05e4b6c2daaf193d425736
William <williamlangroudi@gmail.com>
2018-07-16 21:00:11 +0100

    Add Initial Migrations + Initialize Sqlite db

---
+++ Migrations/20180716195922_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ZestMonitor.Api.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProposalPayments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Amount = table.Column<int>(nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(nullable: false),
+                    ExpectedPayment = table.Column<int>(nullable: false),
+                    ShortDescription = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProposalPayments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProposalPayments");
+        }
+    }
+}
